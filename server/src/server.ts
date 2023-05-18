6f1651942a735e9261c53eee6886bb30eb884acc
Sol Oliveira <sole-oliveira@hotmail.com>
2023-05-18 17:33:01 -0300

    aula 02- install plugin Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(memoriesRoutes)
 
