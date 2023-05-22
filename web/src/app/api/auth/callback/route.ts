0a2dba678dba92e6cb4a6148fb669d61eec37c55
Sol Oliveira <sole-oliveira@hotmail.com>
2023-05-22 13:32:25 -0300

    aula 04 -rota logout

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -12,7 +12,6 @@ export async function GET(request: NextRequest) {
   })
 
   const { token } = registerResponse.data
-  console.log('AQUI ->', registerResponse)
 
   const redirectURL = redirectTo ?? new URL('/', request.url)
 
