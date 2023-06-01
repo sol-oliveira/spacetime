b11b64d615c660d3b5d7138dda62724d1dd6ed14
Sol Oliveira <sole-oliveira@hotmail.com>
2023-05-31 22:17:33 -0300

    aula 05 - criando nova memória

--- web/src/components/NewMemory.tsx
+++ web/src/components/NewMemory.tsx
@@ -1,8 +1,52 @@
+'use client'
+
 import { Camera, ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 import { MediaPicker } from './MediaPicker'
+import { FormEvent } from 'react'
+import { api } from '@/lib/api'
+import Cookie from 'js-cookie'
+import { useRouter } from 'next/navigation'
 
 export function NewMemory() {
+  const router = useRouter()
+
+  async function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    const fileToUpload = formData.get('coverUrl')
+
+    let coverUrl = ''
+
+    if (fileToUpload) {
+      const uploadFormData = new FormData()
+      uploadFormData.set('file', fileToUpload)
+
+      const uploadResponse = await api.post('/upload', uploadFormData)
+
+      coverUrl = uploadResponse.data.fileUrl
+    }
+
+    const token = Cookie.get('token-spacetime')
+
+    await api.post(
+      '/memories',
+      {
+        coverUrl,
+        content: formData.get('content'),
+        isPublic: formData.get('isPublic'),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    router.push('/')
+  }
   return (
     <div className="flex flex-1 flex-col gap-4 p-16">
       <Link
@@ -12,7 +56,10 @@ export function NewMemory() {
         <ChevronLeft className="h-4 w-4" />
         voltar à timeline
       </Link>
-      <form className="flex flex-1 flex-col gap-2">
+      <form
+        onSubmit={handleCreateMemory}
+        className="flex flex-1 flex-col gap-2"
+      >
         <div className="flex items-center gap-4">
           <label
             htmlFor="media"
@@ -45,6 +92,13 @@ export function NewMemory() {
           className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
+
+        <button
+          type="submit"
+          className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+        >
+          Salvar
+        </button>
       </form>
     </div>
   )
